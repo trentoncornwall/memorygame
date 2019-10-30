27c2cc50421378ee0ac8ad82c3916d8aa55f7abf
Trenton Cornwall <trenton434@gmail.com>
2019-10-29 20:24:19 -0600

    pokemone render, correctly tracks clicks, and shuffles pokemon now

--- src/App.js
+++ src/App.js
@@ -5,27 +5,64 @@ import IconCard from "./components/IconCard";
 import Pokemon from "./cardData/pokemon.json";
 
 class App extends Component {
-	state = { score: 0, topScore: 0 };
+  state = { score: 0, topScore: 0, clicked: [], pmon: [] };
 
-	handleBtnClick = e => {
-		this.setState({ score: this.state.score + 1 });
-	};
+  // checks to see if it's been clicked yet and adjust state
+  handleBtnClick = id => {
+    this.state.clicked.includes(id)
+      ? this.alreadyClicked(id)
+      : this.newlyClicked(id);
+  };
 
-	render() {
-		return (
-			<div>
-				<Nav>
-					<ScoreCard score={this.state.score} topScore={this.state.topScore} />
-				</Nav>
-				<IconCard
-					id={Pokemon[0].id}
-					name={Pokemon[0].name}
-					onClick={() => this.handleBtnClick()}
-					img={Pokemon[0].image}
-				/>
-			</div>
-		);
-	}
+  newlyClicked = id => {
+    console.log("NewlyClicked being called with", id);
+
+    this.setState({
+      score: this.state.score + 1,
+      clicked: [...this.state.clicked, id]
+    });
+
+    this.shufflePokemon();
+  };
+
+  alreadyClicked = id => {
+    console.log("already clicked", id, this.state.clicked);
+  };
+
+  shufflePokemon = () => {
+    let newPokemon = [...Pokemon];
+    for (let i = newPokemon.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newPokemon[i], newPokemon[j]] = [newPokemon[j], newPokemon[i]];
+    }
+    this.setState({
+      pmon: newPokemon
+    });
+  };
+  componentDidMount() {
+    this.shufflePokemon();
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Render's score */}
+        <Nav>
+          <ScoreCard score={this.state.score} topScore={this.state.topScore} />
+        </Nav>
+        {/* Creates the Icons */}
+        {this.state.pmon.map(pokemon => (
+          <IconCard
+            key={pokemon.id}
+            id={pokemon.id}
+            name={pokemon.name}
+            onClick={() => this.handleBtnClick(pokemon.id)}
+            img={pokemon.image}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/IconCard/index.js
+++ src/components/IconCard/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./style.css";
 
 function IconCard(props) {
-	return (
-		<div onClick={props.onClick} className="IconCard">
-			<div className="IconImage">
-				<img src={props.img} alt={props.name} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="IconCard" onClick={props.onClick} id={props.id}>
+      <img src={props.img} alt={props.name} />
+    </div>
+  );
 }
 
 export default IconCard;
