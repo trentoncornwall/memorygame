65ebca76cfd9921880c7e3a7a26286466e5cf31b
Trenton Cornwall <trenton434@gmail.com>
2019-11-03 11:59:25 -0700

    Updates

---
+++ precache-manifest.c050c65d50b280ee44fca274eea04df4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99dd4911b3c4c635cb55b14837630e5",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "0a4f30eea6d0deb9b25f",
+    "url": "/memorygame/static/css/main.206a1ebe.chunk.css"
+  },
+  {
+    "revision": "81489e720adcb9a1aa42",
+    "url": "/memorygame/static/js/2.32b00498.chunk.js"
+  },
+  {
+    "revision": "0a4f30eea6d0deb9b25f",
+    "url": "/memorygame/static/js/main.e46e93fd.chunk.js"
+  },
+  {
+    "revision": "5cdb0237536b6187c761",
+    "url": "/memorygame/static/js/runtime-main.1902c3d0.js"
+  }
+]);
